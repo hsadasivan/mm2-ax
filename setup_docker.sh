1cde3a2934a28d7eb50524f67fa62d45c7794cf9
harisankarsadasivan <44511208+harisankarsadasivan@users.noreply.github.com>
2022-09-04 11:24:22 -0400

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,6 +1,11 @@
+#install curl
 sudo apt-get install curl;
+
+#install docker
 curl https://get.docker.com | sh \
   && sudo systemctl --now enable docker;
+  
+#install nvidia container toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list;
@@ -9,12 +14,8 @@ sudo apt-get update;
 sudo apt-get install -y nvidia-docker2;
 sudo systemctl restart docker;
 
-sudo apt-get update; 
 sudo apt-get install -y build-essential;
 sudo apt-get update;
-sudo apt-get install libz-dev;
-sudo apt install zip -y;
-wget https://developer.download.nvidia.com/compute/cuda/11.5.2/local_installers/cuda_11.5.2_495.29.05_linux.run; sudo sh cuda_11.5.2_495.29.05_linux.run --silent
 
 sudo docker pull hariumich/mm2-ax:1.2;
 sudo docker run -it --rm --gpus all --mount type=bind,source=${PWD},target=/mm2-ax/  hariumich/mm2-ax:1.2 bash;
