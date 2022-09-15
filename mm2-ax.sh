047109b934e9f216920e7451190b2515d5be74a0
Harisankar Sadasivan <44511208+harisankarsadasivan@users.noreply.github.com>
2022-09-14 23:02:15 -0400

    Update mm2-ax.sh

--- mm2-ax.sh
+++ mm2-ax.sh
@@ -2,41 +2,41 @@
 
 #script rgoups the reads & invokes mm2-ax binary
 #input: $1 is read length; $2 is path to ONT folder. 
-#For example: ./mm2-ax.sh 2000 ../data/ONT
+#For example: bin/mm2-ax.sh 2000 ../data/ONT
 
 echo "bin length set to $1" ;
 if [[ $1 -eq 2000 ]]
 then	
 	
 	head -n $((211*972*2)) $2/2k.fa > $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205092 --blocks-per-stream=972 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205092 --blocks-per-stream=972 > mm2-ax-$1.log ;
 	
 elif [[ $1 -eq 3000 ]]
 then	
 	
 	head -n $((100*972*2)) $2/3k.fa > $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=97200 --blocks-per-stream=972 >   mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=97200 --blocks-per-stream=972 >   mm2-ax-$1.log ;
 	
 
 elif [[ $1 -eq 4000 ]]
 then	
 	
 	head -n $((85*972*2)) $2/4k.fa > $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=82620 --blocks-per-stream=972 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=82620 --blocks-per-stream=972 > mm2-ax-$1.log ;
 	
 
 elif [[ $1 -eq 5000 ]]
 then	
 	
 	head -n $((64*972*2)) $2/5k.fa > $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=62208 --blocks-per-stream=972 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=62208 --blocks-per-stream=972 > mm2-ax-$1.log ;
 	
 
 elif [[ $1 -eq 10000 ]]
 then	
 	
 	head -n $((39*972*2)) $2/10k.fa > $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=37908 --blocks-per-stream=972 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=37908 --blocks-per-stream=972 > mm2-ax-$1.log ;
 	
 elif [[ $1 -eq 20000 ]]
 then	
@@ -44,7 +44,7 @@ then
 	
 	head -n 18 $2/20k.fa > $2/ip.fa; 
 	head -n $((22917*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206271 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206271 --blocks-per-stream=9 > mm2-ax-$1.log ;
 
 elif [[ $1 -eq 30000 ]]
 then	
@@ -53,7 +53,7 @@ then
 	
 	head -n 18 $2/30k.fa > $2/ip.fa; 
 	head -n $((22913*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206235 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206235 --blocks-per-stream=9 > mm2-ax-$1.log ;
 
 elif [[ $1 -eq 40000 ]]
 then	
@@ -62,7 +62,7 @@ then
 	
 	head -n 18 $2/40k.fa > $2/ip.fa; 
 	head -n $((22903*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206145 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206145 --blocks-per-stream=9 > mm2-ax-$1.log ;
 
 elif [[ $1 -eq 45000 ]]
 then	
@@ -70,7 +70,7 @@ then
 	
 	head -n 18 $2/45k.fa > $2/ip.fa; 
 	head -n $((22901*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206127 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206127 --blocks-per-stream=9 > mm2-ax-$1.log ;
 	
 elif [[ $1 -eq 50000 ]]
 then
@@ -78,21 +78,21 @@ then
 	
 	head -n 18 $2/50k.fa > $2/ip.fa; 
 	head -n $((22894*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206064 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=206064 --blocks-per-stream=9 > mm2-ax-$1.log ;
 elif [[ $1 -eq 100000 ]]
 then	
 	
 	
 	head -n 18 $2/100k.fa > $2/ip.fa; 
 	head -n $((22874*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205884 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205884 --blocks-per-stream=9 > mm2-ax-$1.log ;
 	
 elif [[ $1 -eq 150000 ]]
 then	
 	
 	head -n 18 $2/150k.fa > $2/ip.fa; 
 	head -n $((22818*9*2)) $2/2k.fa >> $2/ip.fa; 
-	./mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205380 --blocks-per-stream=9 > mm2-ax-$1.log ;
+	bin/mm2-ax -t 1 -x map-ont ../data/hg38.mmi ../data/ip.fa --total-no-of-reads=205380 --blocks-per-stream=9 > mm2-ax-$1.log ;
 fi
 
 #generate output from minimap2 v2.17 binary (MAX_SKIP=INF) to visually inspect output equality
